33b7c6f8fb9a4f5e9f5b4050db77cae44c2c9933
erpan14ar@gmail.com <erpan14ar@gmail.com>
2021-04-08 14:14:05 -0700

    updates on vmd scripts

--- vmd/shift_center_of_mass.tcl
+++ vmd/shift_center_of_mass.tcl
@@ -1,7 +1,5 @@
 # shift the center of mass to zero
-set sel [atomselect top all]
-set n [molinfo top get numframes]
-for { set i 0 } { $i < $n } { incr i } {  
+set sel [atomselect top all];set n [molinfo top get numframes];for { set i 0 } { $i < $n } { incr i } {  
   $sel frame $i
   $sel update
   set x [measure center $sel]
